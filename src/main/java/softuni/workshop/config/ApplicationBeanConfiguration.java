bcd94a9b6edb647eaf732d84ca3605637efeb5c6
EnchevIv <ench3viv@gmail.com>
2024-02-25 21:45:16 +0200

    refactor: revert changes to Bcrypt

--- src/main/java/softuni/workshop/config/ApplicationBeanConfiguration.java
+++ src/main/java/softuni/workshop/config/ApplicationBeanConfiguration.java
@@ -3,8 +3,7 @@ package softuni.workshop.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import softuni.workshop.util.XmlParser;
 import softuni.workshop.util.XmlParserImpl;
 
@@ -17,8 +16,8 @@ public class ApplicationBeanConfiguration {
     }
 
     @Bean
-    public PasswordEncoder getPasswordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
